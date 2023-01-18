01b88af0231925ed4245a82255edd6d3d63a07f5
Imran691 <mimran691@gmail.com>
2023-01-18 12:58:56 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,46 @@
+#!usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation, { rainbow } from "chalk-animation";
+
+const sleep = () => new Promise((res, rej) => setTimeout(res, 2000));
+
+async function welcome(){
+    const rainbowTitle = chalkAnimation.rainbow(`***** Let's start counting *****`);
+    await sleep();
+    rainbowTitle.stop()
+}
+
+welcome();
+
+async function askQ(){
+    let Q = await inquirer.prompt([
+        {
+            type: "input",
+            name: "msg",  
+            message: chalk.redBright("Please enter the paragraph"),   
+        }
+    ])
+
+    const paraToArr = Q.msg.split(" ")
+    console.log(`Words in your paragraph are: ${paraToArr.length}`);
+
+    const charsInPara = Q.msg.replace(/ /g, "")  
+    console.log(`Characters in your paragraph are: ${charsInPara.length}`);
+
+}
+
+async function startAgain() {
+    do {
+        await askQ();
+        
+        var playAgain = await inquirer.prompt({
+            type: "input",
+            name: "again",
+            message: chalk.green("Want to count again? Press Y or N"),
+    })
+    } while (playAgain.again === "Y" || playAgain.again === "y");
+}
+
+startAgain();
